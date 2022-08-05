e2ae4f46a4b412a51ed457da5d822aeaf1d73ca8
prashant ranjan <ranjan.prashant@tftus.com>
2022-08-05 13:00:57 +0530

    change

--- src/Components/Placelist.js
+++ src/Components/Placelist.js
@@ -1,33 +1,40 @@
 import React from "react";
+import del from "../Images/del.png";
 
 const PlaceList = (props) => {
-    const onClickHandler=()=>{
-        props.delHandler(props.id)
-    }
+  const onClickHandler = () => {
+    props.delHandler(props.id);
+  };
 
+  if (props.name === "") {
+    return;
+  }
 
-    if (props.name === "") {
-      return;
-    }
-    
-    let image; 
-    try 
-    
-    {const photo_name = props.url.split('.');
-     image = require(`../Images/${photo_name[1]}.png`)}
+  let image;
+  try {
+    const photo_name = props.url.split(".");
 
-    catch {
-      image = require ("../Images/error.png")
-    }
-
+    image = require(`../Images/${photo_name[1]}.png`);
+    // console.log("hello", image);
+  } catch {
+    image = require("../Images/error.png");
+    // console.log("hello", image);
+  }
+  console.log(props)
   return (
     <li>
       <a className="card6" target="_blank" href={props.url}>
-        <img src={props.name + ".png"} alt="" height="25px" width="25px" />
+        <img src={image} alt="" height="25px" width="25px" />
         <span className="card7">{props.name}</span>
       </a>
       {/* <button className="card11" onClick={onClickHandler}>-</button> */}
-      <img src="delete.png"height="20px" width="20px" className="card11" onClick={onClickHandler}></img>
+      <img
+        src={del}
+        height="20px"
+        width="20px"
+        className="card11"
+        onClick={onClickHandler}
+      ></img>
     </li>
   );
 };
